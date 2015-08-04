64924f3b29926d6b32bde7a3cdcab4b45d0a912d
Enol Fernandez <enol.fernandez@egi.eu>
2015-08-04 09:37:31 +0100

    New fedcloud-ui installation script.

---
+++ fedcloud-ui.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+#
+# Installation script for EGI FedCloud client
+# installs and configures VOMS for fedcloud.egi.eu and training.egi.eu VOs
+# installs occi-cli
+#
+
+# Some variables
+VOMSES=/etc/vomses
+VOMSDIR=/etc/grid-security/vomsdir
+SUDO=""
+FETCH_CRL=fetch-crl
+
+# VOMS config
+VOMS1_SERVER=voms1.egee.cesnet.cz
+VOMS1_DN="/DC=org/DC=terena/DC=tcs/OU=Domain Control Validated/CN=voms1.egee.cesnet.cz"
+VOMS1_CA="/C=NL/O=TERENA/CN=TERENA eScience SSL CA"
+VOMS2_SERVER=voms2.grid.cesnet.cz
+VOMS2_DN="/DC=org/DC=terena/DC=tcs/OU=Domain Control Validated/CN=voms2.grid.cesnet.cz"
+VOMS2_CA="/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 2"
+
+
+
+get_root() {
+    if [ $(id -u) -ne 0 ] ; then
+        echo "This needs to be executed as root, e.g. sudo $0"
+        exit 1
+    fi
+}
+
+
+install_darwin() {
+    which brew 2>&1 > /dev/null
+    if [ $? -ne 0 ] ; then
+        echo "Please install homebrew first (http://brew.sh/)"
+	exit 1
+    fi
+
+    echo "*"
+    echo "* Package installation"
+    echo "*"
+    echo ""
+    # install fetch-crl and voms (v2, should work for most cases)
+    brew install fetch-crl voms
+
+    # CA Certificates
+    sudo mkdir -p /etc/grid-security/certificates
+    CA_BUNDLE=https://dist.eugridpma.info/distribution/igtf/current/accredited/igtf-preinstalled-bundle-classic.tar.gz
+    curl -s $CA_BUNDLE | sudo tar -xz -C /etc/grid-security/certificates
+
+    # OCCI client
+    echo "# Missing OCCI client installation!"
+    #ruby -v
+
+#cat > /dev/null << EOF
+#    if which ruby >/dev/null && which gem >/dev/null; then
+#        PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+#    fi
+#EOF
+}
+
+install_debian() {
+    echo "*"
+    echo "* Package installation"
+    echo "*"
+    echo ""
+
+    DIST=$1
+    PSEUDONAME=$2
+ 
+    # ensure that we have curl
+    apt-get -qq install -y curl
+
+    # get repos configurations
+    curl -s http://repository.egi.eu/community/software/rocci.cli/4.3.x/releases/repofiles/$DIST-$PSEUDONAME-amd64.list \
+         -o /etc/apt/sources.list.d/rocci.list
+    echo "deb http://repository.egi.eu/sw/production/cas/1/current egi-igtf core" > \
+         /etc/apt/sources.list.d/egi-trustanchors.list
+   
+    curl -s https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 | \
+         apt-key add -
+
+    curl -s http://repository.egi.eu/sw/production/umd/UMD-DEB-PGP-KEY | \
+         apt-key add -
+
+    curl -s http://repository.egi.eu/sw/production/umd/3/repofiles/debian-squeeze/UMD-3-base.list \
+         -o /etc/apt/sources.list.d/UMD-3-base.list
+
+    curl -s http://repository.egi.eu/sw/production/umd/3/repofiles/debian-squeeze/UMD-3-updates.list \
+         -o /etc/apt/sources.list.d/UMD-3-updates.list 
+
+    apt-get -qq update
+    apt-get -y -q install ca-policy-egi-core fetch-crl occi-cli voms-clients3
+
+    # this is required to make voms-clients3 work
+    [ -e /var/lib/voms-clients3/lib/commons-io.jar ] || \
+        ln -s /usr/share/java/commons-io.jar /var/lib/voms-clients3/lib/
+}
+
+install_rh6() {
+    echo "*"
+    echo "* Package installation"
+    echo "*"
+    echo ""
+    # ensure that we have curl
+    yum -q -y install curl
+    # get repos for UMD (includes CAs) and rOCCI
+    yum -q localinstall -y http://repository.egi.eu/sw/production/umd/3/sl6/x86_64/updates/umd-release-3.0.1-1.el6.noarch.rpm
+    curl -s http://repository.egi.eu/community/software/rocci.cli/4.3.x/releases/repofiles/sl-6-x86_64.repo > \
+         /etc/yum.repos.d/rocci.repo
+
+    # install packages
+    yum -q -y install ca-policy-egi-core fetch-crl occi-cli voms-clients3
+}
+
+setup_voms() {
+    echo ""
+    echo "*"
+    echo "* fedcloud.egi.eu and training.egi.eu VOs configuration"
+    echo "*"
+    echo ""
+
+    set -x
+    $SUDO mkdir -p $VOMSES
+    for VO_PORT in "fedcloud.egi.eu:15002" "training.egi.eu:15014" ; do
+	VO=`echo $VO_PORT | cut -f1 -d":"`
+	PORT=`echo $VO_PORT | cut -f2 -d":"`
+    	$SUDO mkdir -p $VOMSDIR/$VO
+    	$SUDO tee $VOMSDIR/$VO/$VOMS1_SERVER.lsc > /dev/null << EOF
+$VOMS1_DN
+$VOMS1_CA
+EOF
+    	$SUDO tee $VOMSDIR/$VO/$VOMS2_SERVER.lsc > /dev/null << EOF
+$VOMS2_DN
+$VOMS2_CA
+EOF
+
+	echo "\"$VO\" \"$VOMS1_SERVER\" \"$PORT\" \"$VOMS1_DN\" \"$VO\"" | \
+             $SUDO tee $VOMSES/$VO.$VOMS1_SERVER > /dev/null
+	echo "\"$VO\" \"$VOMS2_SERVER\" \"$PORT\" \"$VOMS2_DN\" \"$VO\"" | \
+             $SUDO tee $VOMSES/$VO.$VOMS2_SERVER > /dev/null
+    done
+
+    # fetch crls
+    $SUDO $FETCH_CRL 
+}
+
+
+ 
+if [ -z "$OSTYPE" ] ; then
+    echo "Unable to determine OS, will not install"
+    exit 1
+fi
+
+OS=""
+case "$OSTYPE" in
+    darwin*)
+	# OS X voms uses by default this location:
+	VOMSES=/usr/local/etc/vomses
+	SUDO=sudo
+	FETCH_CRL=/usr/local/sbin/fetch-crl
+	install_darwin
+        ;; 
+    linux*)
+    	get_root
+        if [ -f /etc/redhat-release ] ; then
+            # RH based
+            REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*// | cut -f1 -d"."`
+            if [ "x$REV" == "x6" ] ; then
+                install_rh6     
+            else
+                echo "Unsupported RedHat release $REV"
+                exit 1
+            fi
+        elif [ -f /etc/debian_version ] ; then
+            DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }' | tr '[:upper:]' '[:lower:]'` 
+            PSEUDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
+            REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
+            if [ "x$DIST" == "xdebian" ] ; then
+                if [ "x$PSEUDONAME" != "xwheezy" -a "x$PSEUDONAME" != "xsqueeze" ] ; then
+                    echo "Unsupported debian release $PSEUDONAME"
+                    exit 1
+                fi
+            elif [ "x$DIST" == "xubuntu" ] ; then
+                if [ "x$PSEUDONAME" != "xtrusty" -a "x$PSEUDONAME" != "xprecise" ] ; then
+                    echo "Unsupported ubuntu release $PSEUDONAME"
+                    exit 1
+                fi
+            fi
+            install_debian $DIST $PSEUDONAME $REV
+        else
+            echo "Unsupported Linux distribution"
+            exit 1
+        fi
+        ;;
+    *)
+        echo "Unsupported OS $OSTYPE"
+        exit 1
+        ;;
+esac
+
+setup_voms
+
+cat << EOF
+*******************************************************************************
+occi is now ready to be used. Copy your certificate and key files to ~/.globus
+to create a proxy with:
+
+    voms-proxy-init --voms fedcloud.egi.eu --rfc
+
+set the X509_USER_PROXY variable with
+
+    export X509_USER_PROXY=\`voms-proxy-info -path\`
+
+and start using the occi command:
+
+    occi -e \$ENDPOINT -n x509 -X -x \$X509_USER_PROXY [...]
+
+Check https://wiki.egi.eu/wiki/Federated_Cloud_user_support for more info
+*******************************************************************************
+
+EOF
+
+exit 0
